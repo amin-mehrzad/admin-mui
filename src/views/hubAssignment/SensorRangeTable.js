a08e5f6601714e4d3fb6befb922a2cfd52894604
Amin <amin.mehrzad@gmail.com>
2020-12-14 11:33:55 +0000

    added delete and insert

--- src/views/hubAssignment/SensorRangeTable.js
+++ src/views/hubAssignment/SensorRangeTable.js
@@ -54,6 +54,7 @@ const SensorRangeTable = ({ sensorData, ...rest }) => {
       //  console.log(selectedRow)
       //  setSelectedRow(selectedRow.tableData.id);
        // } )}
+
       options={{
         paging :false,
         search:false,
@@ -63,6 +64,44 @@ const SensorRangeTable = ({ sensorData, ...rest }) => {
           fontSize: '14px'
 
         })
+      }}
+      editable={{
+       onRowAdd: newData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              setData([...data, newData]);
+              
+              resolve();
+            }, 1000)
+          }), 
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve, reject) => {
+            setTimeout(async () => {
+              const dataUpdate = [...data];
+              const index = oldData.tableData.id;
+              dataUpdate[index] = newData;
+              // let hubRoomSensorUpdate = await axios({
+              //   method: 'put',
+              //   url: `http://${process.env.REACT_APP_SERVER_URI}/api/hub-room-sensors`,
+              //  // data: {id:},
+              //   headers: { "Access-Control-Allow-Origin": "*" }
+              // })
+              setData([...dataUpdate]);
+
+              resolve();
+            }, 1000)
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const dataDelete = [...data];
+              const index = oldData.tableData.id;
+              dataDelete.splice(index, 1);
+              setData([...dataDelete]);
+              
+              resolve()
+            }, 1000)
+          }), 
       }}
     />
   )
